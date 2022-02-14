5107166f220962197e8d3585f7148da3062e6df7
mengiefen <mengiefen@gmail.com>
2022-02-14 21:59:02 +0300

    add page navigation

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,16 @@
 import { formattedDate } from "./modules/date.js";
 import { newDate } from "./modules/elements.js";
+import { handleNavigation as navigate } from "./modules/navigation.js";
 // Show current time
 newDate.innerHTML = formattedDate;
+
+// Event listener for 'Add new Button', and Nav Items
+document.body.addEventListener(
+  "click",
+  (event) => {
+    if (event.target.className === "nav-link") {
+      navigate(event.target);
+    }
+  },
+  true
+);
---
+++ scripts/modules/navigation.js
@@ -0,0 +1,20 @@
+import { booklist, addNewBook, contactInfo } from "./elements.js";
+
+// Handles navigation for Single Page Application(SPA)
+export function handleNavigation(target) {
+  if (target.innerHTML === "List") {
+    booklist.style.display = "flex";
+    addNewBook.style.display = "none";
+    contactInfo.style.display = "none";
+  }
+  if (target.innerHTML === "Add new") {
+    booklist.style.display = "none";
+    addNewBook.style.display = "flex";
+    contactInfo.style.display = "none";
+  }
+  if (target.innerHTML === "Contact") {
+    booklist.style.display = "none";
+    addNewBook.style.display = "none";
+    contactInfo.style.display = "flex";
+  }
+}
